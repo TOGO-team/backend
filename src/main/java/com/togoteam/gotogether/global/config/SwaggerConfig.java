61eebae8a3b4509dd84927c76e2b126bb2f1f823
MJJ <m3k0813@gmail.com>
2024-11-14 16:06:25 +0900

    chore: Swagger 세팅

---
+++ src/main/java/com/togoteam/gotogether/global/config/SwaggerConfig.java
@@ -0,0 +1,17 @@
+package com.togoteam.gotogether.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+	@Bean
+	public OpenAPI openAPI() {
+		Info info = new Info().title("같이가요 API").description("같이가요 API 명세서");
+		return new OpenAPI().info(info).components(new Components());
+	}
+}
