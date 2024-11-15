cb49a70aafc632afddc7ddc72cb44fdeeb7d7b0b
MJJ <m3k0813@gmail.com>
2024-11-16 00:39:40 +0900

    feat: Create BaseEntity

---
+++ src/main/java/com/togoteam/gotogether/global/common/BaseEntity.java
@@ -0,0 +1,29 @@
+package com.togoteam.gotogether.global.common;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@MappedSuperclass
+public class BaseEntity {
+
+	@CreatedDate
+	@Column(name = "created_at", updatable = false)
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	@Column(name = "updated_at")
+	private LocalDateTime updatedAt;
+
+	@Column(name = "is_deleted",  columnDefinition = "bit(1) default 0")
+	private boolean deleted;
+}
