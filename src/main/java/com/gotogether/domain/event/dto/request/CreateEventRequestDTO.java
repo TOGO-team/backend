e21f5bc4f1dc8f85708e5f26a1bbbdb2cdeabab8
MJJ <m3k0813@gmail.com>
2024-12-20 03:11:16 +0900

    refactor: 필요없는 import 제거

--- src/main/java/com/gotogether/domain/event/dto/request/CreateEventRequestDTO.java
+++ src/main/java/com/gotogether/domain/event/dto/request/CreateEventRequestDTO.java
@@ -1,55 +1,53 @@
-	package com.gotogether.domain.event.dto.request;
+package com.gotogether.domain.event.dto.request;
 
-	import java.time.LocalDateTime;
-	import java.util.List;
-	import java.util.Map;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
 
-	import com.gotogether.domain.event.entity.Category;
-	import com.gotogether.domain.event.entity.OnlineType;
-	import com.gotogether.domain.hostchannel.entity.HostChannel;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.gotogether.domain.event.entity.Category;
+import com.gotogether.domain.event.entity.OnlineType;
 
-	import lombok.Getter;
+import lombok.Getter;
 
-	import com.fasterxml.jackson.annotation.JsonProperty;
+@Getter
+public class CreateEventRequestDTO {
+	@JsonProperty("hostChannelId")
+	private Long hostChannelId;
 
-	@Getter
-	public class CreateEventRequestDTO {
-		@JsonProperty("hostChannelId")
-		private Long hostChannelId;
+	@JsonProperty("title")
+	private String title;
 
-		@JsonProperty("title")
-		private String title;
+	@JsonProperty("startDateTime")
+	private LocalDateTime startDateTime;
 
-		@JsonProperty("startDateTime")
-		private LocalDateTime startDateTime;
+	@JsonProperty("endDateTime")
+	private LocalDateTime endDateTime;
 
-		@JsonProperty("endDateTime")
-		private LocalDateTime endDateTime;
+	@JsonProperty("bannerImageUrl")
+	private String bannerImageUrl;
 
-		@JsonProperty("bannerImageUrl")
-		private String bannerImageUrl;
+	@JsonProperty("description")
+	private String description;
 
-		@JsonProperty("description")
-		private String description;
+	@JsonProperty("referenceLinks")
+	private Map<String, String> referenceLinks;
 
-		@JsonProperty("referenceLinks")
-		private Map<String, String> referenceLinks;
+	@JsonProperty("onlineType")
+	private OnlineType onlineType;
 
-		@JsonProperty("onlineType")
-		private OnlineType onlineType;
+	@JsonProperty("location")
+	private String location;
 
-		@JsonProperty("location")
-		private String location;
+	@JsonProperty("category")
+	private Category category;
 
-		@JsonProperty("category")
-		private Category category;
+	@JsonProperty("hashtags")
+	private List<String> hashtags;
 
-		@JsonProperty("hashtags")
-		private List<String> hashtags;
+	@JsonProperty("hostEmail")
+	private String hostEmail;
 
-		@JsonProperty("hostEmail")
-		private String hostEmail;
-
-		@JsonProperty("hostPhoneNumber")
-		private String hostPhoneNumber;
-	}
+	@JsonProperty("hostPhoneNumber")
+	private String hostPhoneNumber;
+}
